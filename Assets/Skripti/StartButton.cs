2caf8244df20f20afef0b0f3274cd76acf76bd0a
Ruslans Popovs <bonja1@rambler.ru>
2021-04-26 03:15:26 +0300

    Papildinaju ar jaunu ainu un pogam

---
+++ Assets/Skripti/StartButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartButton : MonoBehaviour
+{
+    public void button()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void button2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
